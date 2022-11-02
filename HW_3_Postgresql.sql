bc8fb7a5063977f73f1fa91cf0b91aa9493ccbb8
Oksana Rak <oksanarak90@gmail.com>
2022-11-02 13:06:00 +0200

    Update file HW_3

--- HW_3_Postgresql.sql
+++ HW_3_Postgresql.sql
@@ -149,37 +149,61 @@ where roles.role_name like 'Senior %';
 
 --Вывести зарплаты Java разработчиков
 select salary.monthly_salary, roles.role_name, employee_salary.salary_id, roles_employee.role_id 
-from salary join roles 
-on salary.id = roles.id
-join employee_salary
-on salary.id = employee_salary.salary_id 
+from employees join employee_salary 
+on employees.id = employee_salary.employee_id 
+join salary
+on employee_salary.salary_id = salary.id 
 join roles_employee
-on roles.id = roles_employee.role_id
+on roles_employee.employee_id = employees.id
+join roles 
+on roles_employee.role_id = roles.id
 where roles.role_name like '% Java developer';
 
 
+--Вывести зарплаты Python разработчиков
 select salary.monthly_salary, roles.role_name, employee_salary.salary_id, roles_employee.role_id 
 from employees join employee_salary 
 on employees.id = employee_salary.employee_id 
 join salary
 on employee_salary.salary_id = salary.id 
 join roles_employee
-on roles.id = roles_employee.role_id
-where roles.role_name like '% Java developer';
+on roles_employee.employee_id = employees.id
+join roles 
+on roles_employee.role_id = roles.id
+where roles.role_name like '% Python developer';
 
 
+--Вывести имена и зарплаты Junior Python разработчиков
+select employees.employee_name, salary.monthly_salary, roles.role_name  
+from employees join salary
+on employees.id = salary.id
+join roles_employee
+on employees.id = roles_employee.employee_id
+join roles 
+on roles_employee.role_id = roles.id
+where roles.role_name like 'Junior Python developer';
 
 
+--Вывести имена и зарплаты Middle JS разработчиков
+select employees.employee_name, salary.monthly_salary, roles.role_name  
+from employees join salary
+on employees.id = salary.id
+join roles_employee
+on employees.id = roles_employee.employee_id
+join roles 
+on roles_employee.role_id = roles.id
+where roles.role_name like 'Middle JavaScript developer';
 
 
-
-
-
-
-
-
-
-
+-- Вывести имена и зарплаты Senior Java разработчиков
+--select employees.employee_name, salary.monthly_salary, roles.role_name  
+--from employees join salary
+--on employees.id = salary.id
+--join roles_employee
+--on employees.id = roles_employee.employee_id
+--join roles 
+--on roles_employee.role_id = roles.id
+--where roles.role_name like 'Senior Java developer';
 
 
 
